8dfee1922ebf0af5a90ce86929219b7d4189abd6
Nitin0202 <nitinsachdeva0202@gmail.com>
2024-09-04 16:45:09 +0530

    removed log form reprint issue solved

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,10 +2,8 @@ const checkHeaderKey = (req, res, next) => {
     const predefinedKey = '1234';
     const userKey = req.headers['key'];
     if (userKey === predefinedKey) {
-        console.log('Header key matches the predefined value.');
         next();
     } else {
-        console.log('Header key does not match the predefined value.');
         return res.status(403).json({ message: 'Forbidden: Invalid header key' });
     }
 };
